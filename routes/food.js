1547debad7cb96251964305cf68bedc5ebba6e6a
Allison Livingston <livingstona16@gmail.com>
2018-05-25 10:49:30 -0600

    Adding query files

--- routes/food.js
+++ routes/food.js
@@ -1,79 +1,38 @@
-// From Dan's Guides: https://github.com/justsml/guides/tree/master/express
-
-// IMPORTANT: CHECK `TODO` NOTICES BELOW!!!
-
-// TODO: Import KNEX CONNECTION OBJECT
-const knex = require('../db/knex') // TODO: Adjust path as needed!
-
-// RESTful Knex Router Template:
+const express = require('express');
+const knex = require('./foodStores')
 const router = module.exports = require('express').Router();
+const queries = require('../queries')
 
-router.get('/', getAll)
-router.get('/:id', getOne)
-router.post('/', create)
-router.put('/:id', update)
-router.delete('/:id', remove)
+router.get("/", (request, response, next) => {
+    queries.list().then(foodStores => {
+        response.json({foodStores});
+    }).catch(next);
+});
 
-// TODO: Don't forget data validation/restrictions:
-// - use regex, mongoose, Joi, bookshelf, *schema lib, etc. many options: choose one
+router.get("/:id", (request, response, next) => {
+    queries.read(request.params.id).then(foodStores => {
+        foodStores
+            ? response.json({foodStores})
+            : response.status(404).json({message: 'Not found'})
+    }).catch(next);
+});
 
-// OPTIONAL/TODO: Move `getQueryOptions` into some shared js file
-function getQueryOptions(query) {
-  let { offset, limit } = query
-  offset = parseInt(offset, null)
-  limit = parseInt(limit, null)
-  offset = offset > 2000 ? 2000 : offset
-  limit = limit > 50 ? 50 : limit
-  return { offset, limit }
-}
+router.post("/", (request, response, next) => {
+    queries.create(request.body).then(foodStores => {
+        response.status(201).json({foodStores: foodStores});
+    }).catch(next);
+});
 
-function getAll(req, res, next) {
-  const { limit, offset } = getQueryOptions(req.query)
-  knex('items')
-    .select('*')
-    .limit(limit)
-    .offset(offset)
-    .then(items => res.status(200).send({ data: items }))
-    .catch(next)
-}
+router.delete("/:id", (request, response, next) => {
+    queries.delete(request.params.id).then(() => {
+        response.status(204).json({deleted: true});
+    }).catch(next);
+});
 
-function getOne(req, res, next) {
-  knex('items')
-    .select('*')
-    .limit(1)
-    .where({ id: req.params.id })
-    .then(([item]) => {
-      if (!item) return res.status(404).send({ message: 'Item not found.' })
-      res.status(200).send({ data: item })
-    })
-    .catch(next)
-}
+router.put("/:id", (request, response, next) => {
+    queries.update(request.params.id, request.body).then(foodStores => {
+        response.json({foodStores: foodStores[0]});
+    }).catch(next);
+});
 
-function create(req, res, next) {
-  // TODO: Validate input data
-  knex('items')
-    .insert(req.body)
-    .then(() => res.status(201).json({ data: req.body }))
-    .catch(next)
-}
-
-function update(req, res, next) {
-  // TODO: Validate input data
-  knex('items')
-    .where({ id: req.params.id })
-    .update(req.body)
-    .then(count => count >= 1
-      ? res.status(200).json({ data: req.body })
-      : res.status(410).json())
-    .catch(next)
-}
-
-function remove(req, res, next) {
-  // TODO: Validate authentication
-  knex('items').where({ id: req.params.id })
-    .delete()
-    .then(count => count >= 1
-      ? res.status(204).json()
-      : res.status(404).json({ message: 'Nothing deleted!' }))
-    .catch(next)
-}
+module.exports = router;
