18bab12c145d588e834d28a9cbf2bab0cf2e1de3
Allison Livingston <livingstona16@gmail.com>
2018-05-25 11:40:58 -0600

    Adding files with seeded db

---
+++ queries.js
@@ -0,0 +1,28 @@
+const database = require('./database-connection')
+
+module.exports = {
+  list() {
+    return database('foodStores').select()
+  },
+  read(id){
+    return database('foodStores').select().where('id', id).first();
+  },
+  create(name){
+    return database('foodStores')
+    .insert(name)
+    .returning('*')
+    .then(record => record[0]);
+  },
+  update(id, name, amount){
+    return database('foodStores')
+    .update(name)
+    .update(amount)
+    .returning('*')
+    .then(record => record[id]);
+  },
+  delete(id){
+    return database('foodStores')
+    .delete()
+    .where('id', id)
+  }
+}
